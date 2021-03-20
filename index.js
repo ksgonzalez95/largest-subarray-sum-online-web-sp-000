392359010216f2204b428ff561a3f7452fab1df2
Kimberly Gonzalez <ksgonzalez95@gmail.com>
2021-03-20 20:19:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-let array = [1, -1, 5, 3, -7, 4, 5, 6, -100, 4]
-
 function largestSubarraySum(array){
  // code to write here
 }
 
-largestSubarraySum(array)
-// 16
+// test cases
+largestSubarraySum([1, -1, 5, 3, -7, 4, 5, 6, -100, 4]) // returns 16
+largestSubarraySum([1, 100, 4, 15, 9, 30]) // returns 159
+largestSubarraySum([1, 100, 4, 15, 9, 30, -1]) // returns 159
+largestSubarraySum([-3, 1, 100, 4, 15, 9, 30]) // returns 159
+largestSubarraySum([-3, 1, 100, 4, 15, 9, 30, -1]) // returns 159
+largestSubarraySum([-2, -3, 4, -1, -2, 1, 5, -3]) // returns 7
+largestSubarraySum([2, 1, -3, 4, -1, 2, 1, -5, 4]) // returns 6
+largestSubarraySum([-1, -1, -5, -3, -7, -4, -5, -6, -100, -4]) // returns 0
+//let timeLimit = 1000;
+//this.timeout(timeLimit)
+let array = new Array(1000).fill(1);
+largestSubarraySum(array) // returns 1000 within the O(n) time limit
+
