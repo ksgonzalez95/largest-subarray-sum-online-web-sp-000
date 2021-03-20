e46f40df178f95440ea1f9b55bf51cd106f23127
Kimberly Gonzalez <ksgonzalez95@gmail.com>
2021-03-20 20:20:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
-function largestSubarraySum(array){
- // code to write here
+function largestSubarraySum(arr){
+    let largestSum = 0
+    let currentSum = 0
+    let negatives = 0
+    let i = 0
+    
+    /*
+    Using a While loop instead of a For loop makes it easy to check more than one element in
+    the array without returning to the top of the loop.  Helpful when implementing a solution in O(n)
+    time. 
+    */ 
+    while(i < arr.length){
+        // This inner while loop handles cases where the array starts with negative numbers
+        while(arr[i]<0){
+            i++
+        }
+        // the next six lines move the summation process forward as long as the elements are positive
+        currentSum += arr[i]
+        i++
+        while(arr[i] && arr[i] > 0){
+            currentSum += arr[i]
+            i++
+        }
+        /*
+        This if statement ensures that the sum of positive subarrays preceded by negatives are stored, and potentially
+        returned as the answer, in the event that the sum of the positive subarray that follows the negative(s) is less than
+        the absolute value of the negative sequence.
+        */
+        if(arr[i] < 0 || i === arr.length){
+            largestSum = Math.max(largestSum, currentSum)
+        }
+        //this while loop stores the sum of subarrays of negative numbers
+        while(arr[i] < 0){
+            negatives += arr[i]
+            i++
+        }
+        /*
+        If the absolute value of the negative subarray is larger the its preceding positive subarray.  Disregard
+        the preceding positive subarray.
+        */ 
+        if(Math.abs(negatives) >= currentSum){
+            currentSum = 0
+            negatives = 0
+        } else {
+            currentSum = currentSum + negatives
+            negatives = 0
+        }
+    }
+    return largestSum
 }
 
-// test cases
-largestSubarraySum([1, -1, 5, 3, -7, 4, 5, 6, -100, 4]) // returns 16
-largestSubarraySum([1, 100, 4, 15, 9, 30]) // returns 159
-largestSubarraySum([1, 100, 4, 15, 9, 30, -1]) // returns 159
-largestSubarraySum([-3, 1, 100, 4, 15, 9, 30]) // returns 159
-largestSubarraySum([-3, 1, 100, 4, 15, 9, 30, -1]) // returns 159
-largestSubarraySum([-2, -3, 4, -1, -2, 1, 5, -3]) // returns 7
-largestSubarraySum([2, 1, -3, 4, -1, 2, 1, -5, 4]) // returns 6
-largestSubarraySum([-1, -1, -5, -3, -7, -4, -5, -6, -100, -4]) // returns 0
-//let timeLimit = 1000;
-//this.timeout(timeLimit)
-let array = new Array(1000).fill(1);
-largestSubarraySum(array) // returns 1000 within the O(n) time limit
