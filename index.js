a0d31183880b352503e93f87200607319e1b8915
Kimberly Gonzalez <ksgonzalez95@gmail.com>
2021-03-20 20:20:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ function largestSubarraySum(arr){
     let currentSum = 0
     let negatives = 0
     let i = 0
-    
+
     /*
     Using a While loop instead of a For loop makes it easy to check more than one element in
     the array without returning to the top of the loop.  Helpful when implementing a solution in O(n)
-    time. 
-    */ 
+    time.
+    */
     while(i < arr.length){
         // This inner while loop handles cases where the array starts with negative numbers
         while(arr[i]<0){
@@ -37,7 +37,7 @@ function largestSubarraySum(arr){
         /*
         If the absolute value of the negative subarray is larger the its preceding positive subarray.  Disregard
         the preceding positive subarray.
-        */ 
+        */
         if(Math.abs(negatives) >= currentSum){
             currentSum = 0
             negatives = 0
@@ -48,4 +48,3 @@ function largestSubarraySum(arr){
     }
     return largestSum
 }
-
